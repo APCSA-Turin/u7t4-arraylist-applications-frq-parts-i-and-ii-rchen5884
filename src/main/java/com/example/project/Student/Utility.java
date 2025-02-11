f7857e9598f7989a27ce3c89447a710db05c60f2
rchen5884 <148903646+rchen5884@users.noreply.github.com>
2025-02-11 22:15:32 +0000

    chall

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -2,16 +2,36 @@ package com.example.project.Student;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Utility {
+import java.util.ArrayList;
 
-    public static ArrayList<Student> sortStudents(ArrayList<Student> list){
-        ArrayList<Student> tempList = new ArrayList<Student>();
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getLastName().compareTo(list.get(i+1).getLastName()) == 0) {
-                
+public class Utility {
+    public static ArrayList<Student> sortStudents(ArrayList<Student> studentsToSort) {
+        int n = studentsToSort.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                Student s1 = studentsToSort.get(j);
+                Student s2 = studentsToSort.get(minIndex);
+                if (s1.getLastName().compareTo(s2.getLastName()) < 0) {
+                    minIndex = j;
+                } 
+                else if (s1.getLastName().equals(s2.getLastName()) &&
+                         s1.getFirstName().compareTo(s2.getFirstName()) < 0) {
+                    minIndex = j;
+                } 
+                else if (s1.getLastName().equals(s2.getLastName()) &&
+                         s1.getFirstName().equals(s2.getFirstName()) &&
+                         s1.getGpa() > s2.getGpa()) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                Student temp = studentsToSort.get(i);
+                studentsToSort.set(i, studentsToSort.get(minIndex));
+                studentsToSort.set(minIndex, temp);
             }
         }
-        return list;
+        return studentsToSort;
     }
-
 }
+
